018423c63616bb91cee2ff06a2a48765e8caba4c
BagasSatriaYn <duhjvdedhvhjufahfy@gmail.com>
2025-09-25 19:06:44 +0700

    Daftar Nama Mahasiswa

---
+++ bin/tugas4.dart
@@ -0,0 +1,15 @@
+import 'package:tugas4/tugas4.dart' as tugas4;
+
+void main(){
+List<String> mahasiswa = [];
+
+mahasiswa.add("Bryan");
+mahasiswa.add("Satria");
+mahasiswa.add("Imel");
+mahasiswa.add("Putri");
+
+print("Daftar Mahasiswa: $mahasiswa");
+
+print("Jumlah Mahasiswa: ${mahasiswa.length}");
+}
+
